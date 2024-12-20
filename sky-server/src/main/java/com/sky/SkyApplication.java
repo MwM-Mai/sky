8ae63d9938bd5d663b0a3c0b3c6ce22761f7bd49
MwM-Mai <104347298+MwM-Mai@users.noreply.github.com>
2024-12-20 18:15:48 +0800

    Update SkyApplication.java

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement //开启注解方式的事务管理
 @EnableScheduling // 启用定时任务
 @Slf4j
-@EnableCaching
+@EnableCaching // spring cache 缓存注解功能
 public class SkyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
